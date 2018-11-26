c78fc32d52086e9f777785d2fabe3703070ff238
Kai Herrmann <kai.herrmann@stiecon.de>
2018-11-26 13:29:10 +0100

    Added current code for server

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Feuermelder
 Wer kennt das nicht, das Haus brennt, du bist nicht zuhause und kannst die Feuerwehr nicht rufen.
 Mit unserer praktischen Anwendung bekommst du eine Benachrichtung, und du entscheidest selbst zwischen Versicherung und Feuerwehr
+
+### Informationen:
+Wir benötigen die DMT.h Libary
+Die DMT Libary hat nicht funktioniert, daher haben wir auf version 1.2.3 gedowngraded.
+https://github.com/adafruit/DHT-sensor-library/issues/62 
---
+++ temperatur/temperatur.ino
@@ -0,0 +1,109 @@
+
+#include <DHT.h>;
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+
+
+#define DHTPIN 13 // pin we're connected to
+#define DHTTYPE DHT22
+DHT dht(DHTPIN, DHTTYPE); 
+const char* ssid = "iPhoneX Kai 2018";
+const char* password = "test12345";
+
+ESP8266WebServer server(80);
+
+String data;
+
+
+//Variables
+int chk;
+float hum;  //humidity 
+float temp; //temperature
+
+void handleRoot() {
+  digitalWrite(led, 1);
+  server.send(200, "text/plain", "hello from esp8266!");
+  digitalWrite(led, 0);
+}
+
+void handleNotFound() {
+  digitalWrite(led, 1);
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  for (uint8_t i = 0; i < server.args(); i++) {
+    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+  }
+  server.send(404, "text/plain", message);
+  digitalWrite(led, 0);
+}
+
+void getTempHumValues(){
+  hum = dht.readHumidity();
+  temp = dht.readTemperature();
+
+  String httpmessage = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE HTML>\r\n<html>";
+  httpmessage += temp;
+  httpmessage += "</html>";
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  pinMode(2, OUTPUT);
+  digitalWrite(2, 0);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.println("");
+    while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  server.on("/", handleRoot);
+
+  sever.on("/temp", getTempHumValues);
+
+/*  server.begin();
+  if(WiFi.status() != WL_CONNECTED){
+    Serial.println("Error with WiFI Connection");
+  }*/
+  //Serial.println(WiFi.localIP()); //Gebt diese an die andere Gruppe
+  //dht.begin();
+}
+
+void loop()
+{
+  server.handleClient();
+  /*
+  WiFiClient client = server.available();
+  delay(2000);
+  hum = dht.readHumidity();
+  temp = dht.readTemperature();
+  client.flush();
+  String httpmessage = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE HTML>\r\n<html>";
+  httpmessage += temp;
+  httpmessage += "</html>";
+  client.print(httpmessage);
+  //Print temp and humidity values
+  Serial.print("Humidity: ");
+  Serial.print(hum);
+  Serial.print(" %, Temp: ");
+  Serial.print(temp);
+  Serial.println(" Celsius");
+  delay(15000); //Delay 2 sec.*/
+}
+
+   
